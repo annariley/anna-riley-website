208311483de8e1d5fabba0c0d17dfff06540577e
annariley <annamriley4@gmail.com>
2022-10-04 14:38:22 +0200

    Updates

---
+++ precache-manifest.2a636296f5d5a574cd5817041337ed01.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cc17181f715ca313dd7dfc08a161922",
+    "url": "/anna-riley-website/index.html"
+  },
+  {
+    "revision": "0c118c9b3cad86d8f5f1",
+    "url": "/anna-riley-website/static/css/main.1173eda8.chunk.css"
+  },
+  {
+    "revision": "d19c0134add5c9fad394",
+    "url": "/anna-riley-website/static/js/2.7bb5ae5f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/anna-riley-website/static/js/2.7bb5ae5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c118c9b3cad86d8f5f1",
+    "url": "/anna-riley-website/static/js/main.9d4eb3f9.chunk.js"
+  },
+  {
+    "revision": "d618e13e928ff7946fea",
+    "url": "/anna-riley-website/static/js/runtime-main.69ab6499.js"
+  },
+  {
+    "revision": "89bc18fb0af64d1fbd15feece43d7683",
+    "url": "/anna-riley-website/static/media/Lunch_atop_a_Skyscraper_-_Charles_Clyde_Ebbets.89bc18fb.jpg"
+  },
+  {
+    "revision": "489e820b7592052ad74062a20e78caa1",
+    "url": "/anna-riley-website/static/media/clarertt.489e820b.JPG"
+  },
+  {
+    "revision": "036ccfc640163d4a4b3d1d8bd206feab",
+    "url": "/anna-riley-website/static/media/favourite.036ccfc6.jpg"
+  },
+  {
+    "revision": "4ed3d09da2851d35759abdaf20a1d2a2",
+    "url": "/anna-riley-website/static/media/me.4ed3d09d.jpg"
+  }
+]);
